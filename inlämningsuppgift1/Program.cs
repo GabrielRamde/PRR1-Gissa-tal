2b024d7d663a7db5391d0cdc689adb6c57da5dc4
GabrielRamde <54704377+GabrielRamde@users.noreply.github.com>
2019-09-10 13:27:08 +0200

    Lagt upp manuellt

---
+++ inlämningsuppgift1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Inlämningsuppgift1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random generator = new Random();
+            int randomNumber = generator.Next(1, 100);
+            int number = 0;
+            int i = 0;
+            while (number != randomNumber)
+            {
+                Console.WriteLine("Skriv ett tal mellan 1 till 100: ");
+                number = int.Parse(Console.ReadLine());
+                i++;
+                if (number > randomNumber)
+                    Console.WriteLine($"{number} är för högt");
+                else if (number < randomNumber)
+                    Console.WriteLine($"{number} är för lågt");
+                else
+                    Console.WriteLine($"Du gissade rätt, talet var {randomNumber}. Det tog {i} försök!");
+            }
+        }
+    }
+}
